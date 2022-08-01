67308c6c76d31ebfbba69cf3eaf180ba29580e5c
frogy9cat <ramzeek_voker@mail.ru>
2022-08-02 04:10:53 +0500

    BGRbot_1.1

--- handlers/users/pic_url_get.py
+++ handlers/users/pic_url_get.py
@@ -1,13 +1,12 @@
-# from aiogram import types
-# from loader import dp
-# from utils.getting_picture import get_picture
-# from filters.PrivateFilter import IsPrivate
-# from data.config import ADMINS
+from aiogram import types
+from loader import dp
+from utils.getting_picture import get_picture
+from filters.PrivateFilter import IsPrivate
+from data.config import ADMINS
 
 
-# @dp.message_handler(IsPrivate())
-# async def from_url_get_pic(message: types.Message):
-#     if message.from_user.id in ADMINS != True
-#         await message.answer_photo(photo=get_picture(str(message.text)))
+@dp.message_handler(IsPrivate())
+async def from_url_get_pic(message: types.Message):
+    await message.answer_photo(photo=get_picture(str(message.text)))
 
         
