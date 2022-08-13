af0af8df95cc2a43b8e8cd90140dd72a5354e673
frogy9cat <109206156+frogy9cat@users.noreply.github.com>
2022-08-13 10:37:47 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,4 +4,4 @@ import os
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
 ADMINS = str(os.environ.get("ADMINS")).split()  # adminlar ro'yxati
 IP = str(os.environ.get("ip"))  # Xosting ip manzili
-API_KEY = str(os.environ.get("API_KEY"))
+#API_KEY = str(os.environ.get("API_KEY"))
